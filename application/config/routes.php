c4a913930c9c3675524ee4d97169d7c4d5906aca
rainhanifa <vj2daworld@gmail.com>
2017-07-10 10:59:56 +0700

    update data in database, update routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-$route['member/(:any)'] = "member/$1";
-$route['(:any)'] = "home/$1";

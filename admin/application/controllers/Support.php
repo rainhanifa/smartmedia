c2c88003bd4ef5cc4ba6b064eabbd6344368467a
naayunir <naayunir@gmail.com>
2017-07-10 16:08:14 +0700

    update last _up

--- admin/application/controllers/Support.php
+++ admin/application/controllers/Support.php
@@ -103,7 +103,7 @@
             }
 		}
 		public function ticket($id=0){
-			$data['ticket'] = $this->db->query('SELECT tickets.id, tickets.date_ticket, departments.name_department, tickets.subject_ticket, tickets.status_ticket from departments JOIN tickets ON departments.id_department = tickets.department_id ')->result_array();
+			$data['ticket'] = $this->db->query('SELECT DISTINCT tickets.id, tickets.id_ticket,  min(tickets.date_ticket) as date_lawas, max(tickets.date_ticket) as date_terbaru, departments.name_department, tickets.subject_ticket, tickets.status_ticket from departments JOIN tickets ON departments.id_department = tickets.department_id group by tickets.id_ticket')->result_array();
 			$this->load->view('template/header-admin.php');
 			$this->load->view('template/navbar-admin.php');
 			$this->load->view('support/tickets.php', $data);
--- member/application/controllers/Dashboard.php
+++ member/application/controllers/Dashboard.php
@@ -4,9 +4,10 @@
 
 		public function index()
 		{
+			$data['ticket'] = $this->db->query('SELECT tickets.id, tickets.date_ticket, departments.name_department, tickets.subject_ticket, tickets.status_ticket from departments JOIN tickets ON departments.id_department = tickets.department_id ')->result_array();
 			$this->load->view('template/header-member.php');
 			$this->load->view('template/navbar-member.php');
-			$this->load->view('dashboard/index.php');
+			$this->load->view('dashboard/index.php', $data);
 			$this->load->view('template/footer-member.php');
 		}
 	}

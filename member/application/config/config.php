21c8946b45c0878c4dd3423fc5b1541e9c477f58
Ibnu Suhaemy <ibnuspeedster@gmail.com>
2017-07-18 14:19:15 +0700

    Test

--- member/application/config/config.php
+++ member/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/smartmedia/member';
+$config['base_url'] = 'http://localhost:8080/smartmedia/member';
 
 /*
 |--------------------------------------------------------------------------
